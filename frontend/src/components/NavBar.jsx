60f315e150fb126b7ad5e4651944c32b1927528d
KevinLavigne <lavigne.kevin02@gmail.com>
2022-07-21 18:01:30 +0200

    navbar done

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import { HashLink as NavLink } from "react-router-hash-link";
 import { useContext } from "react";
 import ExportContext from "../contexts/Context";
-import Formulaire from "./Counter";
+import Formulaire from "./Formulaire";
 
 function NavBar() {
   const { handleForm, isFormOpen } = useContext(ExportContext.Context);
   return (
     <>
       {isFormOpen && <Formulaire />}
-      <ul>
+      <ul className="flex justify-between py-5 px-10 text-3xl bg-gris mb-8">
         <NavLink to="/">
           <li>Accueil</li>
         </NavLink>
@@ -18,9 +18,10 @@ function NavBar() {
         <button type="button" onClick={() => handleForm()}>
           <li>Contactez-moi</li>
         </button>
-        <NavLink to="/Page3">
-          <li>Page3</li>
-        </NavLink>
+        <select className="bg-gris">
+          <option value="1"> FR </option>
+          <option value="2"> EN </option>
+        </select>
       </ul>
     </>
   );
